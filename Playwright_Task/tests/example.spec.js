f0613c27dd8b171dabaf0bb1a4e108881ba835ad
Runzhou Zhu <runzhouz@metropolia.fi>
2025-04-05 16:33:08 +0300

    123

--- Playwright_Task/tests/example.spec.js
+++ Playwright_Task/tests/example.spec.js
@@ -20,27 +20,6 @@ test("get started link", async ({ page }) => {
   ).toBeVisible();
 });
 
-// Does the site respect the reduced motion settings?
-// https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion
-test("respect the reduced motion settings", async ({ page }) => {
-  await page.goto("https://www.ilry.fi/en/");
-
-  // Check the reduced motion setting.
-  const prefersReducedMotion = await page.evaluate(() => {
-    return window.matchMedia("(prefers-reduced-motion: reduce)").matches;
-  });
-  const animation = await page.evaluate(() => {
-    return window.getComputedStyle(document.body).getPropertyValue("animation");
-  });
-
-  // If prefers-reduced-motion is true, animation should be none.
-  if (prefersReducedMotion) {
-    expect(animation).toBe("none");
-  } else {
-    expect(animation).not.toBe("none");
-  }
-});
-
 // Select the main menu item and Perform visual regression testing on the main menu
 test("Perform visual regression testing on the main menu", async ({ page }) => {
   await page.goto("https://www.ilry.fi/en/");
---
+++ Playwright_Task/tests/reduced_motion.spec.js
@@ -0,0 +1,21 @@
+// Does the site respect the reduced motion settings?
+// https://developer.mozilla.org/en-US/docs/Web/CSS/@media/prefers-reduced-motion
+
+test("respect the reduced motion settings", async ({ page }) => {
+  await page.goto("https://www.ilry.fi/en/");
+
+  // Check the reduced motion setting.
+  const prefersReducedMotion = await page.evaluate(() => {
+    return window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+  });
+  const animation = await page.evaluate(() => {
+    return window.getComputedStyle(document.body).getPropertyValue("animation");
+  });
+
+  // If prefers-reduced-motion is true, animation should be none.
+  if (prefersReducedMotion) {
+    expect(animation).toBe("none");
+  } else {
+    expect(animation).not.toBe("none");
+  }
+});
