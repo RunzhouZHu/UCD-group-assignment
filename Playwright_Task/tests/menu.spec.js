d0b7878feaf59b20362c6946ba512c8d4f9a10e7
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-05 13:07:42 +0300

    Preparing visual regression of the menu.

--- Playwright_Task/tests/menu.spec.js
+++ Playwright_Task/tests/menu.spec.js
@@ -13,7 +13,17 @@ test("Main menu links should be validated", async () => {
   const mainMenuLinks = await getLinksFromMenu(page);
   for (const link of mainMenuLinks) {
     const response = await page.goto(link);
-    await expect(response.status()).toBe(200);  
+    expect(response.status()).toBe(200);  
     }
   await browser.close();
+});
+// Select the main menu item and Perform visual regression testing on the main menu
+test("Perform visual regression testing on the main menu", async ({ page }) => {
+  await page.goto("https://www.ilry.fi/en/");
+
+  // Select the main menu item
+  page.click("span.nav-toggle__text");
+
+  // Check that the URL has changed
+  await expect(page).toHaveScreenshot({ maxDiffPixels: 100 });
 });
