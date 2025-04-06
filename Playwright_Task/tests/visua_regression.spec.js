3a3742de77af39505ddb8634885091a929dd858a
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-06 19:04:29 +0300

    Preparing to fix implementation.

--- Playwright_Task/tests/visua_regression.spec.js
+++ Playwright_Task/tests/visua_regression.spec.js
@@ -1,12 +1,4 @@
 import { test, expect } from "@playwright/test";
-// Select the main menu item and Perform visual regression testing on the main menu
-
-const getLinksFromMenu = async (page) => {
-  const mainMenuLinks = await page.$$eval("nav a", (links) =>
-    links.map((link) => link.href)
-  );
-  return mainMenuLinks;
-};
 
 test("Perform visual regression testing on the main menu", async ({ page }) => {
   await page.goto("https://www.ilry.fi/en/");
