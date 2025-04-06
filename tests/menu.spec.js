f3676ea6985231b1e20d0cdb8091ca40fcf5049e
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-06 19:26:16 +0300

    Added role selectors.

--- tests/menu.spec.js
+++ tests/menu.spec.js
@@ -2,18 +2,18 @@ import { test, expect } from "@playwright/test";
 import { firefox } from "playwright";
 
 const getLinksFromMenu = async (page) => {
-    const mainMenuLinks = await page.$$eval('nav a', links => links.map(link => link.href));
-    return mainMenuLinks;
-}
+  const nav = page.getByRole('navigation');
+  const links = await nav.getByRole('link').all();
+  const hrefs = await Promise.all(links.map(link => link.getAttribute('href')));
+  return hrefs.filter(Boolean);
+};
 
-test("Main menu links should be validated", async () => {
-  const browser = await firefox.launch();
-  const page = await browser.newPage();
+test("Main menu links should be validated", async ({ page }) => {
   await page.goto('https://www.ilry.fi/en/');
   const mainMenuLinks = await getLinksFromMenu(page);
   for (const link of mainMenuLinks) {
     const response = await page.goto(link);
-    expect(response.status()).toBe(200);  
-    }
-  await browser.close();
+    expect(response.status()).toBe(200);
+  }
 });
+
