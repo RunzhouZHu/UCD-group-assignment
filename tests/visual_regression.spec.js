aa4c40da5272e1b233a89e9d4ef36a512eaf40cf
Minimal-Alexi <alexpop@metropolia.fi>
2025-04-07 07:56:17 +0300

    Added more role selectors.

--- tests/visual_regression.spec.js
+++ tests/visual_regression.spec.js
@@ -3,13 +3,15 @@ import { test, expect } from "@playwright/test";
 test("Perform visual regression testing on the main menu", async ({ page }) => {
   await page.goto("https://www.ilry.fi/en/");
 
-  page.click("span.nav-toggle__text");
-  const menu = page.locator('nav[aria-label="Main menu"]');
+  await page.getByRole('button', { name: /menu/i }).click();
+  const menu = page.getByRole('navigation', { name: "Main menu" });
   await expect(menu).toBeVisible();
+
   const a11ySnapshot = await page.accessibility.snapshot({ interestingOnly: true });
   const roles = a11ySnapshot.children.map(child => child.role);
   console.log("Accessibility roles in the main menu:", roles);
   expect(roles.length).toBeGreaterThan(0);
+  
   const dynamicElements = [
     page.locator('span.b-changing-text__suffix.transition-out')
   ];
